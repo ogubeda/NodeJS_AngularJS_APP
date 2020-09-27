5187c60ab82289f97dabc4181f3bea565c30414e
og.ubeda <og.ubeda@gmail.com>
2020-09-27 21:24:18 +0200

    Cambios en gulpfile para soporte de css y imágenes

--- frontend/src/js/home/homeSlider.component.js
+++ frontend/src/js/home/homeSlider.component.js
@@ -3,9 +3,9 @@ class homeSliderCtrl {
         this.myInterval = 5000;
         this.noWrapSlides = false;
 
-        this.slides = [ {image:'https://data.whicdn.com/images/307144997/original.jpg?t=1518442689',text:"Get funding for your code projects.", id: 0},
-                    {image:'/images/portada1.png',text:"Supports other projects to keep the community growing.", id: 1},
-                    {image:'/images/portada3.jpg',text:"Other users can do the code you don't know how to do.", id: 2}];
+        this.slides = [ {image:'images/portada1.png',text:"Play it what you want.", id: 0},
+                    {image:'images/portada2.jpg',text:"When you want.", id: 1},
+                    {image:'images/portada3.jpg',text:"And where you want.", id: 2}];
     }
 }
 

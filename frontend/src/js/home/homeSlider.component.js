6506f62be7cd1a035fef40a4f68caf0d26d0dfb1
og.ubeda <og.ubeda@gmail.com>
2020-09-28 13:11:09 +0200

    Cambios en css

--- frontend/src/js/home/homeSlider.component.js
+++ frontend/src/js/home/homeSlider.component.js
@@ -3,7 +3,7 @@ class homeSliderCtrl {
         this.myInterval = 5000;
         this.noWrapSlides = false;
 
-        this.slides = [ {image:'images/portada1.png',text:"Play it what you want.", id: 0},
+        this.slides = [ {image:'images/portada1.png',text:"Listen what you want.", id: 0},
                     {image:'images/portada2.jpg',text:"When you want.", id: 1},
                     {image:'images/portada3.jpg',text:"And where you want.", id: 2}];
     }

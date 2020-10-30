22153adeefea70e14265b3416e751dffd0831d53
og.ubeda <og.ubeda@gmail.com>
2020-10-30 15:22:58 +0100

    Creación model group

---
+++ graphql/graphql/schemas/groups/group.schema.js
@@ -0,0 +1,20 @@
+import { gql } from 'apollo-server-express';
+
+const typeDefs = gql `
+    extend type Query {
+        group(slug: String!): Group
+        groups: [Group]
+    }
+    type Group {
+        id: ID!
+        slug: String!
+        name: String
+        singers: [String]
+        creationDate: String
+        albums: [String]
+        image: String
+        favoritesCount: Number
+    }
+`;
+
+export default typeDefs;

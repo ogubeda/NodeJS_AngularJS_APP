bc8192a0fbf1e0c8ed4d39efd924c045e3da8f4a
og.ubeda <og.ubeda@gmail.com>
2020-11-01 18:17:31 +0100

    Dockerización de los dos backends

--- backend/graphql/app.js
+++ backend/graphql/app.js
@@ -43,7 +43,7 @@ function mongooseConnect() {
       mongoose.set('useNewUrlParser', true);
       mongoose.set('useUnifiedTopology', true);
       try {
-        mongoose.connect('mongodb://localhost/conduit_nodejs');
+        mongoose.connect('mongodb://mongo:27017/conduit_nodejs');
       } catch (error) {
         console.log(error);
         
--- backend/rest/app.js
+++ backend/rest/app.js
@@ -37,7 +37,7 @@ if (!isProduction) {
 if(isProduction){
   mongoose.connect(process.env.MONGODB_URI);
 } else {
-  mongoose.connect('mongodb://mongo/conduit_nodejs');
+  mongoose.connect('mongodb://mongo:27017/conduit_nodejs');
   mongoose.set('debug', true);
 }
 

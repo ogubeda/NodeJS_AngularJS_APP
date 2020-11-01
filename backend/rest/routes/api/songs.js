ccc5b823214918d4c2028812c7ad0cd8b8a60d5e
og.ubeda <og.ubeda@gmail.com>
2020-11-01 20:04:29 +0100

    Migración await songs

--- backend/rest/routes/api/songs.js
+++ backend/rest/routes/api/songs.js
@@ -20,203 +20,171 @@ router.param('song', async function (req, res, next, slug) {
   }// end_catch
 });
 
-router.param('comment', function(req, res, next, id) {
-  Comment.findById(id).then(function(comment){
-    if(!comment) { return res.sendStatus(404); }
+router.param('comment', async function(req, res, next, id) {
+  try {
+    let comment = await Comment.findById(id);
 
+    if (!comment) return res.sendStatus(404); 
     req.comment = comment;
 
     return next();
-  }).catch(next);
+  }catch(e) {
+    next();
+  }// end_catch
 });
 
-router.get("/", auth.optional, function (req, res, next) {
+router.get("/", auth.optional, async function (req, res, next) {
   var query = {};
   var limit = 20;
   var offset = 0;
 
   let order = req.query.order ? {[req.query.order[0]]: req.query.order[1]} : {createdAt: 'desc'};
 
-  if(typeof req.query.limit !== 'undefined'){
-    limit = req.query.limit;
-  }
+  if(typeof req.query.limit !== 'undefined') limit = req.query.limit;
 
-  if(typeof req.query.offset !== 'undefined'){
-    offset = req.query.offset;
-  }
+  if(typeof req.query.offset !== 'undefined') offset = req.query.offset;
 
-  if( typeof req.query.tag !== 'undefined' ){
-    query.tagList = {"$in" : [req.query.tag]};
-  }
+  if( typeof req.query.tag !== 'undefined' ) query.tagList = {"$in" : [req.query.tag]};
   
+  try {
+    let uploaded = req.query.uploaded ? await User.findOne({username: req.query.uploaded}) : null;
+    let favoriter = req.query.favorited ? await User.findOne({username: req.query.favorited}) : null;
 
-  Promise.all([
-    req.query.uploaded ? User.findOne({username: req.query.uploaded}) : null,
-    req.query.favorited ? User.findOne({username: req.query.favorited}) : null
-  ]).then(function(results){
-    var uploaded = results[0];
-    var favoriter = results[1];
+    if(uploaded) query.uploaded = uploaded._id;
 
-    if(uploaded){
-      query.uploaded = uploaded._id;
-    }
+    if(favoriter) query._id = {$in: favoriter.favorites};
+    else if(req.query.favorited) query._id = {$in: []};
 
-    if(favoriter){
-      query._id = {$in: favoriter.favorites};
-    } else if(req.query.favorited){
-      query._id = {$in: []};
-    }
+    let songs = await Song.find(query).limit(Number(limit)).skip(Number(offset)).sort(order).populate('uploaded').exec();
+    let songsCount = await Song.count(query).exec();
+    let user = req.payload ? await User.findById(req.payload.id) : null;
 
-    return Promise.all([
-      Song.find(query)
-        .limit(Number(limit))
-        .skip(Number(offset))
-        .sort(order)
-        .populate('uploaded')
-        .exec(),
-      Song.count(query).exec(),
-      req.payload ? User.findById(req.payload.id) : null,
-    ]).then(function(results){
-
-      var songs = results[0];
-      var songsCount = results[1];
-      var user = results[2];
-
-      return res.json({
-        songs: songs.map(function(song){
-          return song.toJSONFor(user);
-        }),
-        songsCount: songsCount
-      });
-    });
-  }).catch(next);
-
+    return res.json({
+      songs: songs.map(function(song) {
+        return song.toJSONFor(user);
+      }), songsCount: songsCount
+    })
+  }catch(e) {
+    next();
+  }// end_catch
 });
 
-router.get('/feed', auth.required, function(req, res, next) {
+router.get('/feed', auth.required, async function(req, res, next) {
   var limit = 20;
   var offset = 0;
 
-  if(typeof req.query.limit !== 'undefined'){
-    limit = req.query.limit;
-  }
+  if(typeof req.query.limit !== 'undefined') limit = req.query.limit;
 
-  if(typeof req.query.offset !== 'undefined'){
-    offset = req.query.offset;
-  }
+  if(typeof req.query.offset !== 'undefined') offset = req.query.offset;
 
-  User.findById(req.payload.id).then(function(user){
-    if (!user) { return res.sendStatus(401); }
+  try {
+    let user = await User.findById(req.payload.id);
 
-    Promise.all([
-      Song.find({ uploaded: {$in: user.following}})
-        .limit(Number(limit))
-        .skip(Number(offset))
-        .populate('uploaded')
-        .exec(),
-      Song.count({ uploaded: {$in: user.following}})
-    ]).then(function(results){
-      var songs = results[0];
-      var songsCount = results[1];
+    if (!user) return res.sendStatus(401);
 
-      return res.json({
-        songs: songs.map(function(song){
-          return song.toJSONFor(user);
-        }),
-        songsCount: songsCount
-      });
-    }).catch(next);
-  });
+    let songs = await Song.find({ uploaded: {$in: user.following}}).limit(Number(limit)).skip(Number(offset)).populate('uploaded').exec();
+    let songsCount = await Song.count({ uploaded: {$in: user.following}});
+
+    return res.json({
+      songs: songs.map(function(song){
+        return song.toJSONFor(user);
+      }),
+      songsCount: songsCount
+    });
+  }catch(e) {
+    next();
+  }// end_catch
 });
 
-router.get("/taglist", function (req, res, next) {
-  Song.distinct('tagList')
-    .then(function (tagList) {
-      return res.json({ tagList: tagList })
-    }).catch(next);
+router.get("/taglist", async function (req, res, next) {
+  try {
+    let tagList = await Song.distinct('tagList');
+    return res.json({tagList: tagList});
+  }catch(e) {
+    next();
+  }// end_catch
 });
 
-router.get("/:song", auth.optional,function (req, res, next) {
-
-  Promise.all([
-    req.payload ? User.findById(req.payload.id) : null,
-    req.song.populate('uploaded').execPopulate()
-  ]).then(function(results) {
-    let user = results[0];
+router.get("/:song", auth.optional, async function (req, res, next) {
+  try {
+    let user = req.payload ? await User.findById(req.payload.id) : null;
+    await req.song.populate('uploaded').execPopulate();
 
     return res.json({song: req.song.toJSONFor(user)});
-  }).catch(next);
+  }catch(e) {
+    next();
+  }// end_catch
 });
 
-router.post("/", auth.required, function (req, res, next) {
-  User.findById(req.payload.id).then(function (user) {
-    if (!user) { return res.sendStatus(401); }
+router.post("/", auth.required, async function (req, res, next) {
+  try {
+    let user = await User.findById(req.payload.id);
+    if (!user) return res.sendStatus(401);
 
-    var song = new Song(req.body.song);
-
+    let song = new Song(req.body.song);
     song.uploaded = user;
+    await song.save();
 
-    return song
-      .save()
-      .then(function () {
-        res.json({ song: song.toJSONFor(user) });
-      })
-      .catch(next);
-  })
+    return res.json({song:song.toJSONFor(user)})
+  }catch(e) {
+    next();
+  }// end_catch
 });
 
 
-router.delete("/:song", auth.required, function (req, res, next) { //search by slug
-  Promise.all([
-    User.findById(req.payload.id),
-    Song.findOne({slug: req.params.song}).populate('uploaded')
-  ]).then(function(results) {
-    let user = results[0];
-    let userUpload = results[1].toJSONFor().uploaded;
+router.delete("/:song", auth.required, async function (req, res, next) { //search by slug
+  try {
+    let user = await User.findById(req.payload.id);
+    let userUpload = await Song.findOne({slug: req.params.song}).populate('uploaded').toJSONFor().uploaded;
 
     if (user.username === userUpload.username) {
-      
-      if (songUtils.deleteSong(req.song, user)) {
-          return res.sendStatus(204);
-      }
+      if (songUtils.deleteSong(req.song, user)){
+        return res.sendStatus(204);
+      }// end_if
     }else return res.sendStatus(403);
-  }).catch(next);
+  }catch(e) {
+    next();
+  }// end_catch
 });
 
 router.post('/:song/favorite', auth.required, async function (req, res, next) {
   var songId = req.song._id;
 
-  User.findById(req.payload.id).then(async function (user) {
-    if (!user) { return res.sendStatus(401); }
+  try {
+    let user = await User.findById(req.payload.id);
+    if (!user) return res.sendStatus(401);
 
-    return user.favorite(songId).then(async function () {
-      await userUtils.moreReputation(user, 20);
-      return req.song.updateFavoriteCount().then(function (song) {
-        return res.json({ song: song.toJSONFor(user)});
-        // return res.json({ song: song });
-      });
-    });
-  }).catch(next);
+    await user.favorite(songId);
+    await userUtils.moreReputation(user, 20);
+    let song = await req.song.updateFavoriteCount();
+
+    return res.json({song: song.toJSONFor(user)})
+  }catch(e) {
+    next()
+  }// end_catch
 });
 
-router.delete('/:song/favorite', auth.required, function (req, res, next) {
+router.delete('/:song/favorite', auth.required, async function (req, res, next) {
   var songId = req.song._id;
 
-  User.findById(req.payload.id).then(function (user) {
-    if (!user) { return res.sendStatus(401); }
+  try {
+    let user = await User.findById(req.payload.id);
+    if (!user) return res.sendStatus(401);
 
-    return user.unfavorite(songId).then(async function () {
-      await userUtils.lessReputation(user, 20);
-      return req.song.updateFavoriteCount().then(function (song) {
-        return res.json({ song: song.toJSONFor(user)});
-      });
-    });
-  }).catch(next);
+    await user.unfavorite(songId);
+    await userUtils.lessReputation(user, 20);
+    let song = await req.song.updateFavoriteCount();
+
+    return res.json({song: song.toJSONFor(user)});
+  }catch(e) {
+    next();
+  }// end_catch
 });
 
-router.get('/:song/comments', auth.optional, function(req, res, next){
-  Promise.resolve(req.payload ? User.findById(req.payload.id) : null).then(function(user){
-    return req.song.populate({
+router.get('/:song/comments', auth.optional, async function(req, res, next) {
+  try {
+    let user = req.payload ? await User.findById(req.payload.id) : null;
+    let song = await req.song.populate({
       path: 'comments',
       populate: {
         path: 'author'
@@ -226,46 +194,46 @@ router.get('/:song/comments', auth.optional, function(req, res, next){
           createdAt: 'desc'
         }
       }
-    }).execPopulate().then(function(song) {
-      return res.json({comments: req.song.comments.map(function(comment){
-        return comment.toJSONFor(user);
-      })});
-    });
-  }).catch(next);
+    }).execPopulate();
+    return res.json({comments: req.song.comments.map(function(comment){
+        return comment.toJSONFor(user)})});
+  }catch(e) {
+    next();
+  }// end_catch
 });
 
-router.post('/:song/comments', auth.required, function(req, res, next) {
-  User.findById(req.payload.id).then(function(user){
-    if(!user){ return res.sendStatus(401); }
+router.post('/:song/comments', auth.required, async function(req, res, next) {
+  try {
+    let user = await User.findById(req.payload.id);
+    if (!user) res.sendStatus(401);
 
-    var comment = new Comment(req.body.comment);
+    let comment = new Comment(req.body.comment);
     comment.song = req.song;
     comment.author = user;
+    await comment.save();
 
-    return comment.save().then(function(){
-      req.song.comments = req.song.comments.concat([comment]);
+    req.song.comments = req.song.comments.concat([comment]);
 
-      return req.song.save().then(async function(song) {
-        await userUtils.moreReputation(user, 40)
-        res.json({comment: comment.toJSONFor(user)});
-      });
-    });
-  }).catch(next);
+    await req.song.save();
+    await userUtils.moreReputation(user, 40);
+
+    return res.json({comment: comment.toJSONFor(user)});
+  }catch(e) {
+    next();
+  }// end_catch
 });
 
 router.delete('/:song/comments/:comment', auth.required, async function(req, res, next) {
   if(req.comment.author.toString() === req.payload.id.toString()){
-    req.song.comments.remove(req.comment._id);
+    await req.song.comments.remove(req.comment._id);
 
     user = await User.findById(req.payload.id);
 
     await userUtils.lessReputation(user, 40);
+    await req.song.save();
+    await Comment.find({_id: req.comment._id}).remove().exec();
 
-    req.song.save()
-      .then(Comment.find({_id: req.comment._id}).remove().exec())
-      .then(function(){
-        res.sendStatus(204);
-      });
+    res.sendStatus(204);
   } else {
     res.sendStatus(403);
   }
